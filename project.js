ea2f258d4c8cbb70709970d95e6d7a1975b9a115
Jaideep-alt <jaideepbhojakb@gmail.com>
2023-10-08 15:10:28 +0530

    Added method in project.js

--- project.js
+++ project.js
@@ -2,3 +2,7 @@ const methodToAdd = (params) =>{
     const sumValues = params.a + params.b;
     console.log(sumValues);
 }
+
+const methodToDo = () =>{
+    console.log("New Push from git to github.");
+}
